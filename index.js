e9900ae0375826a93156a164b0efa538cd56e72c
sara01romao <sara-romao@live.com>
2020-08-25 16:16:43 -0300

    calculo

---
+++ index.js
@@ -0,0 +1,30 @@
+const altura =1.56;
+const peso= 70.00;
+
+
+const imc = peso/(altura*altura);
+
+console.log(imc);
+
+
+if (imc <= 18.5) {
+
+    console.log("seu IMC é classificado como MAGREZA");
+
+} else if (imc >= 18.5 && imc <= 24.9) {
+
+    console.log("seu IMC é classificado como NORMAL");
+
+
+} else if (imc >= 25 && imc <= 29.9) {
+
+    console.log("seu IMC é classificado como SOBREPESO");
+
+} else if (imc >= 30 && imc <= 39.9) {
+
+    console.log("seu IMC é classificado como OBESIDADE");
+
+} else {
+    console.log("seu IMC é classificado como OBESSIDADE GRAVE");
+
+}
