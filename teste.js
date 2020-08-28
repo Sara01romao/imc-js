deaba25615c0a59f14b51aa02b189e5b8a76ba21
sara01romao <sara-romao@live.com>
2020-08-27 23:50:39 -0300

    .

---
+++ teste.js
@@ -0,0 +1,22 @@
+
+  function somarValores(){
+    var s1 = document.getElementById("s1").value;
+    var s2 = document.getElementById("s2").value;
+    var s3 = parseInt(s1) + parseInt(s2);
+    alert(s3);
+
+  }
+
+
+
+function calcular(){
+    var n1= document.getElementById('n1').value;
+    var n2= document.getElementById('n2').value;
+    var soma =parseInt(n1)+parseInt(n2);
+    
+
+    document.getElementById('result').innerHTML.value= soma;
+  
+   
+
+}
