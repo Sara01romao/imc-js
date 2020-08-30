22178c3066979921dc249bb752d6a073809bc350
sara01romao <sara-romao@live.com>
2020-08-29 21:58:54 -0300

    .

--- index.js
+++ index.js
@@ -8,6 +8,34 @@ function calcular(){
     imc=imc.toFixed(2)//pegar duas casas depois da vígula
    
     document.getElementById('result').value= imc;
+
+
+
+
+    // document.getElementById('msg').innerHTML= "sara";//escrever mgs no label
+    
+
+    if (imc <= 18.5) {
+
+      document.getElementById('msg').innerHTML="Seu IMC é classificado como MAGREZA";
+      
+       } else if (imc >= 18.5 && imc <= 24.9) {
+      
+        document.getElementById('msg').innerHTML="Seu IMC é classificado como NORMAL";
+      
+      
+       } else if (imc >= 25 && imc <= 29.9) {
+      
+        document.getElementById('msg').innerHTML="Seu IMC é classificado como SOBREPESO";
+      
+       } else if (imc >= 30 && imc <= 39.9) {
+      
+        document.getElementById('msg').innerHTML="Seu IMC é classificado como OBESIDADE";
+      
+       } else {
+        document.getElementById('msg').innerHTML="Seu IMC é classificado como OBESSIDADE GRAVE";
+      
+       }
   
   }
 
@@ -16,15 +44,14 @@ function calcular(){
     document.getElementById('peso').value=' ';
     document.getElementById('altura').value=' ';
     document.getElementById('result').value=' ';
+    document.getElementById('msg').innerHTML= ' ';
     
-  }
-
-
+  } //limpa campos
 
 
 
   $(document).ready(function(){
-		$("#peso").mask("00.00");
+		$("#peso").mask("#0.000", {reverse: true});
 		$("#altura").mask("0.00");
 	});// para mascara dos inputs
 
